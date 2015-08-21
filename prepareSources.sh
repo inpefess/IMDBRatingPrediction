1447fc4140d9b002d8fe98cbdf14229f431d5da2
Boris Shminke <schminkeba@gmail.com>
2015-08-21 10:19:21 -0400

    Beta version of data preparation script

--- prepareSources.sh
+++ prepareSources.sh
@@ -1,6 +1,6 @@
-sed 1,1d actors.list | sed 1,1d > actors.dat
-sed 1,1d actresses.list | sed 1,1d > actresses.dat
-sed 1,1d directors.list | sed 1,1d > directros.dat
-sed 1,1d genres.list | sed 1,1d > genres.dat
-sed 1,1d ratings.list | sed 1,1d > ratings.dat
-sed 1,1d writers.list | sed 1,1d > writers.dat
+head -n -272 actors.list | tail -n +240 > actors.dat
+head -n -107 actresses.list | tail -n +242 > actresses.dat
+head -n -209 directors.list | tail -n +236 > directros.dat
+tail -n +381 genres.list > genres.dat
+head -n -147 ratings.list | tail -n +297 > ratings.dat
+head -n -124 writers.list | tail -n +303 > writers.dat
