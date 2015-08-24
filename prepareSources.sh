ac3cc382ab9124274e297dd6ec4209e4b209ddcb
Boris Shminke <schminkeba@gmail.com>
2015-08-24 23:31:26 +0300

    Decided to use IMDbPy

--- prepareSources.sh
+++ prepareSources.sh
@@ -1,6 +1,4 @@
-head -n -272 actors.list | tail -n +240 > actors.dat
-head -n -107 actresses.list | tail -n +242 > actresses.dat
-head -n -209 directors.list | tail -n +236 > directros.dat
-tail -n +381 genres.list > genres.dat
-head -n -147 ratings.list | tail -n +297 > ratings.dat
-head -n -124 writers.list | tail -n +303 > writers.dat
+# before this "sudo pip install IMDbPy"
+# SQLite 3.7 or later is the fastest
+# --sqlite-transactions - that is extremely important for speed
+imdbpy2sql.py -d sources -u sqlite:/path/imdb.sqlite --sqlite-transactions
