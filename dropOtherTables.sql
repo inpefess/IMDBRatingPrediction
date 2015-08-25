e06c06143cd7a4138f6ce96d43bf683be5a3e7ce
Boris Shminke <schminkeba@gmail.com>
2015-08-25 10:28:18 -0400

    Too many tables

---
+++ dropOtherTables.sql
@@ -0,0 +1,12 @@
+drop table aka_name;
+drop table aka_title;
+drop table comp_cast_type;
+drop table company_type;
+drop table complete_cast;
+drop table keyword;
+drop table link_type;
+drop table movie_companies;
+drop table movie_keyword;
+drop table movie_link;
+drop table person_info;
+
