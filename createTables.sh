01988152aca51293393b21bc4b3f9420eccec0a3
Boris Shminke <schminkeba@gmail.com>
2015-08-25 05:37:01 -0400

    splitted one big SQL file to several scripts

---
+++ createTables.sh
@@ -0,0 +1,2 @@
+sqlite3 imdb.sqlite < movies.sql
+sqlite3 imdb.sqlite < roles.sql
---
+++ roles.sql
@@ -0,0 +1,9 @@
+;
+
+drop table roles;
+create table roles as
+select case when role_id in (1,2) then 1
+else role_id end role, person_id, movie_id
+from cast_info
+where movie_id in (select movie_id from movies)
+;
