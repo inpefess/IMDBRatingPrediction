2675fc2752f1853d53800ba7fad6106d2096299b
Boris Shminke <schminkeba@gmail.com>
2015-08-25 05:30:05 -0400

    changed sources

--- getSources.sh
+++ getSources.sh
@@ -2,7 +2,7 @@ IMDBUrl="ftp://ftp.fu-berlin.de/pub/misc/movies/database"
 rm -r sources
 mkdir sources
 cd sources
-for name in $(cat sourceNames)
+for name in $(cat ../sourceNames)
 do
 	wget $IMDBUrl/${name}.list.gz
 done
--- prepareSources.sh
+++ prepareSources.sh
@@ -1,4 +1,4 @@
 # before this "sudo pip install IMDbPy"
 # SQLite 3.7 or later is the fastest
 # --sqlite-transactions - that is extremely important for speed
-imdbpy2sql.py -d sources -u sqlite:/path/imdb.sqlite --sqlite-transactions
+imdbpy2sql.py -d sources -u sqlite:./imdb.sqlite --sqlite-transactions
