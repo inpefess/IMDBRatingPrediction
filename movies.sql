b69e21ee34a6f77728e21c04ccc3700fb88d433b
Boris Shminke <schminkeba@gmail.com>
2015-08-30 16:34:09 +0300

    not every movie has cast info

--- movies.sql
+++ movies.sql
@@ -1,11 +1,18 @@
 drop table movies;
 create table movies as
-select cast(info as number) rating, movie_id
+select
+    cast(info as number) rating,
+    movie_id
 from movie_info_idx
 where info_type_id = 101
 and movie_id in (
-select movie_id
-from movie_info_idx
-where info_type_id = 100
-and cast(info as integer) >= 11000)
+    select movie_id
+    from movie_info_idx
+    where info_type_id = 100
+    and cast(info as integer) >= 10000
+)
+and movie_id in (
+    select movie_id
+    from cast_info
+)
 ;
