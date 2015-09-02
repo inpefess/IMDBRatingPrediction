f925535bb301a199e9aa37e791b341d7772ae371
inpefess <schminkeba@gmail.com>
2015-09-02 09:26:09 -0400

    added some header

--- prepareSources.sh
+++ prepareSources.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 # before this "sudo pip install IMDbPy"
 # SQLite 3.7 or later is the fastest
 # --sqlite-transactions - that is extremely important for speed
-imdbpy2sql.py -d sources -u sqlite:./imdb.sqlite --sqlite-transactions
+imdbpy2sql.py -d sources -u sqlite:./imdb_raw.db --sqlite-transactions
