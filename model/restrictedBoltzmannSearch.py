24d5e12ff534ea88df015fc97b653c5797e7865d
Boris Shminke <schminkeba@gmail.com>
2015-09-10 14:07:42 +0000

    very slow version

--- model/restrictedBoltzmannSearch.py
+++ model/restrictedBoltzmannSearch.py
@@ -5,27 +5,41 @@ c.close()
 del c
 del apsw
 
-x = [x.split(',') for (x, y) in movie_data]
+X = [x.split(',') for (x, y) in movie_data]
 y = [y for (x, y) in movie_data]
 del movie_data
 
-from sklearn.feature_extraction import FeatureHasher
-fh = FeatureHasher(input_type = "string")
-X = fh.transform(x)
-del x
-del fh
-del FeatureHasher
-
-import xgboost as xgb
 from sklearn.pipeline import Pipeline
+from xgboost import XGBRegressor
+from sklearn.feature_extraction import FeatureHasher
+from sklearn.neural_network import BernoulliRBM
+thePipe = Pipeline([
+    ("hash", FeatureHasher(input_type = "string")),
+    ('RBM', BernoulliRBM()),
+    ('XGB', XGBRegressor())
+])
+
 from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import mean_squared_error, make_scorer
-from sklearn.neural_network import BernoulliRBM
 
-xgbRegr = xgb.XGBRegressor()
-clf = GridSearchCV(Pipeline([('RBM', BernoulliRBM()), ('XGB', xgbRegr)]),
-    {'XGB__max_depth': [1], 'XGB__n_estimators': [20]}, verbose=1, n_jobs = -1,
-    scoring = make_scorer(mean_squared_error, greater_is_better = False), cv = 3)
+paramGrid = {
+    'XGB__max_depth': [3],
+    'XGB__n_estimators': [100],
+    'RBM__n_components': [20],
+    "hash__n_features": [100000]
+}
+
+theScorer = make_scorer(mean_squared_error, greater_is_better = False)
+
+clf = GridSearchCV(
+    thePipe,
+    paramGrid,
+    verbose = 2,
+    n_jobs = -1,
+    scoring = theScorer,
+    cv = 3
+)
+
 clf.fit(X, y)
 print(clf.best_score_)
 print(clf.best_params_)
